d509bdab4ccb999c551d4576ea63d24d2d84184a
Spicurt <richmondhilldarreng@gmail.com>
2020-10-29 15:26:46 -0400

    commands

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-//variables
-const discord = require("discord.js");
-const client = new discord.Client();
-const cmdPrefix = "s!";
-//const config = require('./config.json');
+const discord = require("discord.js")
+const client = new discord.Client()
+
+const config = require("./config.json")
+const command = require("./commands")
 var version = "0.6.2 - Alpha";
    
 client.on('ready', () =>{
-    console.log("This bot is currently online");
+    console.log("This bot is currently online")
+
+    command(client, "hi", (message) =>{
+        message.channel.send('hi')
+    })
 })
 
 
-client.login(process.env.botToken);
+client.login(config.token)
 
